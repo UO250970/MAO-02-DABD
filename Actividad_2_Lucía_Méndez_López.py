890eeb66d0fbcea9d3d63a86990cfec4cdc11733
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2024-05-11 20:25:11 +0200

    Update Actividad_2_Lucía_Méndez_López.py

--- Actividad_2_Lucía_Méndez_López.py
+++ Actividad_2_Lucía_Méndez_López.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pickle
-import timm
+# import timm
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
@@ -10,13 +10,13 @@ from langchain.llms import CTransformers
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-from transformers import AutoModelForCausalLM
+# from transformers import AutoModelForCausalLM
 
-from huggingface_hub import hf_hub_url
-from transformers import GPT2LMHeadModel
+# from huggingface_hub import hf_hub_url
+# from transformers import GPT2LMHeadModel
 
 
-from huggingface_hub import ModelHubMixin
+# from huggingface_hub import ModelHubMixin
 
 # this function is responsible for splitting the data into smaller chunks and convert the data in document format
 def chunks_and_document(txt):
@@ -42,19 +42,19 @@ def chunks_and_document(txt):
         
    # return llm
 
-def load_llm():
+# def load_llm():
     # Obtener la URL del modelo utilizando el ID del modelo
     # model_url = hf_hub_url(model_username, model_filename)
     
     # Construir la URL del modelo utilizando la ruta del archivo en GitHub
-    model_url = f"https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/raw/main/llama-2-7b-chat.ggmlv3.q4_0:8daa9615cce30c259a9555b1cc250d461d1bc69980a274b44d7eda0be78076d8"
+   #  model_url = f"https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/raw/main/llama-2-7b-chat.ggmlv3.q4_0:8daa9615cce30c259a9555b1cc250d461d1bc69980a274b44d7eda0be78076d8"
 
-    model_reloaded = timm.create_model('hf_hub:Soondra/llama-model.bin', pretrained=True)
+   #  model_reloaded = timm.create_model('hf_hub:Soondra/llama-model.bin', pretrained=True)
 
     # Cargar el modelo utilizando la URL
-    model_llm = GPT2LMHeadModel.from_pretrained(model_reloaded)
+   #  model_llm = GPT2LMHeadModel.from_pretrained(model_reloaded)
     
-    return model_llm
+   #  return model_llm
 
 #llm_model_id = "Soondra/llama-model.bin"  
 #llm_model_id = "TheBloke/Llama-2-7B-Chat-GGML"
@@ -65,8 +65,15 @@ def load_llm():
 # model_username = "TheBloke"  # Reemplaza esto con el nombre de usuario del autor del modelo
 # model_filename = "Llama-2-7B-Chat-GGML"  # Reemplaza esto con el nombre del archivo del modelo
 
-model_username = "TheBloke/Llama-2-7B-Chat-GGML"
-model_filename = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+# model_username = "TheBloke/Llama-2-7B-Chat-GGML"
+# model_filename = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+
+def load_llm(model_path):
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+    
+model_path = "saved_models\llama-model.bin"
 
 # this functions is used for applying the llm model with our document 
 def chains_and_response(docs):
