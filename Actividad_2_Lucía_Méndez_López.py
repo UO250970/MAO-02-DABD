200d925d2427cf452add12281d81c130432e88af
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2024-05-11 20:06:27 +0200

    Update Actividad_2_Lucía_Méndez_López.py

--- Actividad_2_Lucía_Méndez_López.py
+++ Actividad_2_Lucía_Méndez_López.py
@@ -11,6 +11,8 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 from transformers import AutoModelForCausalLM
 
+import replicate
+
 from huggingface_hub import hf_hub_url
 from transformers import GPT2LMHeadModel
 
@@ -45,7 +47,7 @@ def load_llm():
     # model_url = hf_hub_url(model_username, model_filename)
     
     # Construir la URL del modelo utilizando la ruta del archivo en GitHub
-    model_url = f"https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/raw/main/"
+    model_url = f"https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/raw/main/llama-2-7b-chat.ggmlv3.q4_0:8daa9615cce30c259a9555b1cc250d461d1bc69980a274b44d7eda0be78076d8"
 
     # Cargar el modelo utilizando la URL
     model_llm = GPT2LMHeadModel.from_pretrained(model_url)
