260c379bbe8bd8674c7914bd8bde39307ae8a66f
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2024-05-11 16:02:59 +0200

    Update Actividad_2_Lucía_Méndez_López.py

--- Actividad_2_Lucía_Méndez_López.py
+++ Actividad_2_Lucía_Méndez_López.py
@@ -3,8 +3,6 @@
 
 #st.title('Técnicas de desarrollo de aplicaciones de Big Data')
 
-pip install streamlit
-
 import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
