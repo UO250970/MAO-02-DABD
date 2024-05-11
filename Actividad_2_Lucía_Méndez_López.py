7952a4a9cc21e1237d3c41c27dd23747c7b96f12
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2024-05-11 15:25:20 +0200

    Update Actividad_2_Lucía_Méndez_López.py

--- Actividad_2_Lucía_Méndez_López.py
+++ Actividad_2_Lucía_Méndez_López.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+from langchain.text_splitter import CharacterTextSplitter # used for splitter the text into smalle chunks
+from langchain.docstore.document import Document # convert the chunks in document format
+from langchain.chains.summarize import load_summarize_chain # connect prompt and llm model
+from langchain import PromptTemplate # for creating prompt 
+from langchain.llms import CTransformers # loading the llm model
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
