b4af86be94f591e941da25ab116dee19fa5db4b4
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2024-05-12 15:16:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,9 +82,9 @@ with st.form('summarize_form', clear_on_submit=False):
             #response = summarization_pipeline(txt_input)
             summarizer = load_llm()
             response = summarizer(txt_input, max_length=200, min_length=30, do_sample=False)
-            resumen = response[0]['summary_text'].replace("[", "").replace("]", "")
+            resumen = response[0]['summary_text']
             result.append(resumen)
 
 if len(result):
     col2.subheader('Tu texto resumido aquí')
-    col2.info(result)
+    col2.info(result[0])
