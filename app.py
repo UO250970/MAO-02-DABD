4f2b0e586696aea490f736ba61869fa1d80e1149
Lucía Méndez <32478830+UO250970@users.noreply.github.com>
2024-05-12 13:41:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ with st.form('summarize_form', clear_on_submit=False):
             #docs = chunks_and_document(txt_input)
             #response = chains_and_response(docs)
             #response = summarization_pipeline(txt_input)
-            response = summarizer(txt_input, max_length=1000, min_length=30, do_sample=False))
+            response = summarizer(txt_input, max_length=1000, min_length=30, do_sample=False)
             result.append(response)
 
 if len(result):
